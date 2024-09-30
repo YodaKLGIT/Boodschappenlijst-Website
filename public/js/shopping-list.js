3f279b90d1d7cbde5a82a6576ccd640a93e7715d
bartgro2 <akiraenishi@gmail.com>
2024-09-30 13:08:26 +0200

    moved js code into javascript file,  added notifaction when list doesn't exist

---
+++ public/js/shopping-list.js
@@ -0,0 +1,28 @@
+// custom.js
+
+// Function to toggle product visibility
+function toggleProducts(event) {
+    event.preventDefault(); // Prevent the default anchor behavior
+    const productsDiv = event.target.closest('li').querySelector('.products');
+
+    if (productsDiv) {
+        const isVisible = productsDiv.style.maxHeight !== '0px';
+        
+        // If it's currently visible, hide it
+        if (isVisible) {
+            productsDiv.style.maxHeight = '0'; // Start collapsing
+            productsDiv.style.opacity = '0'; // Fade out
+        } else {
+            productsDiv.style.maxHeight = `${productsDiv.scrollHeight}px`; // Expand to full height
+            productsDiv.style.opacity = '1'; // Fade in
+        }
+    }
+}
+
+// Function to dismiss notification
+function dismissNotification() {
+    const notification = document.getElementById('notification');
+    if (notification) {
+        notification.remove(); 
+    }
+}
