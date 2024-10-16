251155909e62f9b7ecf666bb68ccf191b9bac76a
bartgro2 <akiraenishi@gmail.com>
2024-10-16 13:30:35 +0200

    made adjustments

--- database/migrations/2024_10_16_105435_create_themes_table.php
+++ database/migrations/2024_10_16_105435_create_themes_table.php
@@ -6,22 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('themes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('strap_color');
+            $table->string('body_color');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('themes');
     }
 };
+
