e5b5d35475e23fdc77dd37dc906d148552847c53
Stijn Vallentgoed <183837@novacollege.nl>
2024-10-23 13:45:39 +0200

    removed

--- resources/views/shoppinglists/show.blade.php
+++
@@ -1,9 +0,0 @@
-<form action="{{ route('shoppinglist.invite', $shoppinglist) }}" method="POST">
-    @csrf
-    <select name="user_id">
-        @foreach($users as $user)
-            <option value="{{ $user->id }}">{{ $user->name }}</option>
-        @endforeach
-    </select>
-    <button type="submit">Invite User</button>
-</form>
