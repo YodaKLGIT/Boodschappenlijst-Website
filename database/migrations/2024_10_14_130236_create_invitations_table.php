4c212d6f62fc5ec9b037c042ae8fa82e39e903dd
Stijn Vallentgoed <183837@novacollege.nl>
2024-10-25 10:28:59 +0200

    error gefixt

--- database/migrations/2024_10_14_130236_create_invitations_table.php
+++ database/migrations/2024_10_14_130236_create_invitations_table.php
@@ -16,6 +16,7 @@ class CreateInvitationsTable extends Migration
         Schema::create('invitations', function (Blueprint $table) {
             $table->id(); // Primary key
             $table->foreignId('shoppinglist_id')->constrained()->onDelete('cascade'); // Foreign key with cascade delete
+            $table->foreignId('recipient_id')->constrained('users')->onDelete('cascade'); // Add recipient_id foreign key
             $table->string('email'); // Email of the invited user
             $table->timestamps(); // created_at and updated_at
         });
