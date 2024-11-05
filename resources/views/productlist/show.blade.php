bfe7299829e723969369dc87ab97403ac0b18ac3
bartgro2 <akiraenishi@gmail.com>
2024-11-05 11:16:11 +0100

    removed weird uncessary code

--- resources/views/productlist/show.blade.php
+++ resources/views/productlist/show.blade.php
@@ -10,17 +10,7 @@
     </div>
 
     <div class="container mx-auto px-4 py-8 mb-16">
-        @if($productlist->products->isEmpty())
-            <div class="bg-white rounded-lg shadow-md p-6 max-w-md mx-auto">
-                <p class="text-gray-600 mb-4">No products available in this list.</p>
-                <a href="{{ route('products.create') }}" class="inline-flex items-center gap-2 rounded-lg bg-pink-600 px-4 py-2 text-white hover:bg-pink-700 transition-colors duration-300">
-                    <span class="text-sm">Add a new product</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                    </svg>
-                </a>
-            </div>
-        @else
+        
             <div class="flex flex-col lg:flex-row">
                 <!-- Card Section -->
                 <div class="lg:w-full">
@@ -91,7 +81,6 @@
                     </div>
                 </div>
             </div>
-        @endif <!-- Closing the if statement for checking if products are empty -->
     </div>
 </x-app-layout>
 
